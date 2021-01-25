eae75ee7f66a6a37e4471c73ce1306b652d7199a
Petter Tondel <petter.toendel@outlook.com>
2021-01-25 17:17:08 +0100

    Commit #2. La til en print setning

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("Endring2"); //Se om dette kommer med etter push
     }
 }
