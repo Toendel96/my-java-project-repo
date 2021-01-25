c244ccd7bbb11689b0e32538a36bc1bdeb55ad38
Petter Tondel <petter.toendel@outlook.com>
2021-01-25 17:24:14 +0100

    Commit #3. La til en print setning etter ny branch ble laget

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("Endring2"); //Se om dette kommer med etter push
+
+        System.out.println(("Se om ny branch får med endring, mens master branch står untoched"));
     }
 }
